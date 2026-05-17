2bc4a6e8b8f5a9bcad61276907f01918d26e06a9
reporeserve <reporeserve@gmail.com>
2026-05-17 09:35:35 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1264,3 +1264,13 @@ document.addEventListener("keydown", (event) => {
     closeCookieModal();
   }
 });
+
+document.querySelectorAll(".sol-card").forEach((card) => {
+  card.addEventListener("mousemove", (e) => {
+    const rect = card.getBoundingClientRect();
+    const x = ((e.clientX - rect.left) / rect.width) * 100;
+    const y = ((e.clientY - rect.top) / rect.height) * 100;
+    card.style.setProperty("--sol-mx", x + "%");
+    card.style.setProperty("--sol-my", y + "%");
+  });
+});
